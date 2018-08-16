5eaf2d86ddb3f1f6b63a12200ac9e007767a7909
matejbolko <36517473+matejbolko@users.noreply.github.com>
2018-08-16 23:38:07 +0200

    offline sw.js
    added offline mode - serviceWorker, more @ https://developers.google.com/web/fundamentals/primers/service-workers/

--- js/main.js
+++ js/main.js
@@ -200,6 +200,13 @@ addMarkersToMap = (restaurants = self.restaurants) => {
   });
 
 }
+/* https://developers.google.com/web/fundamentals/primers/service-workers/registration */
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js');
+  });
+}
+
 /* addMarkersToMap = (restaurants = self.restaurants) => {
   restaurants.forEach(restaurant => {
     // Add marker to the map
---
+++ sw.js
@@ -0,0 +1,51 @@
+/*https://developers.google.com/web/fundamentals/primers/service-workers/#install_a_service_worker*/
+var CACHE_NAME = 'restaurant-stage-v1';
+var urlsToCache = [
+  '/',
+  './index.html',
+  '/restaurant.html',
+  '/css/styles.css',
+  '/data/restaurants.json',
+  '/js/dbhelper.js',
+  '/js/main.js',
+  '/js/restaurant_info.js'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheNames != CACHE_NAME) {
+            return caches.delete(cacheNames);
+          }
+        })
+      );
+    })
+  );
+});
